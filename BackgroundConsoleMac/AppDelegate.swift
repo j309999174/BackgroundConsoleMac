9fe8644391a7891606b1099bb002887b19ef0098
江东 <jiangdong@jiangdongdemac.local>
2018-03-26 22:46:21 +0800

    Initial Commit

---
+++ BackgroundConsoleMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BackgroundConsoleMac
+//
+//  Created by 江东 on 2018/3/26.
+//  Copyright © 2018年 江东. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BackgroundConsoleMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BackgroundConsoleMac
+//
+//  Created by 江东 on 2018/3/26.
+//  Copyright © 2018年 江东. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
